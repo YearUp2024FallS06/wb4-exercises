bca6930346ab6e60caf4f343e5f4edd8bc72e491
mattchristenson <matt@rcc.team>
2024-10-22 12:16:12 -0500

    Implemented GetTotalPay the old way

---
+++ HotelOperations/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+public class Employee {
+
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private float hoursWorked;
+
+
+    public Employee(int employeeId, String name, String department, double payRate, float hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getTotalPay(){
+        if(hoursWorked > 40){
+            float overtimehours = hoursWorked - 40;
+            double regularPay = 40 * payRate;
+            double overtimePay = overtimehours * payRate * 1.5;
+            return regularPay + overtimePay;
+        }
+        else {
+            return this.payRate * this.hoursWorked;
+        }
+
+    }
+
+    public float getRegularHours(){
+        return 0;
+    }
+
+
+    public float getOvertimeHours(){
+        return 0;
+    }
+
+
+}
